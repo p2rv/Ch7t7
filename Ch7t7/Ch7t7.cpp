7a45c1ef938392967a8f533f5b6389dd2d973a02
Pavel P. (book) <pros_pavlo@mail.ru>
2016-01-07 22:23:27 +0800

    Create main declarations and define function getDepth

--- Ch7t7/Ch7t7.cpp
+++ Ch7t7/Ch7t7.cpp
@@ -6,9 +6,35 @@
 
 #include "Ch7t7.h"
 
+TNode* root=NULL;
+TNode* current=NULL;
+int treeLength=0;
+
 int main()
 {
 	std::cout << "ttest";
 	std::cin.get();
 	return 0;
+}
+
+bool addNode(TNode* tNode) {
+	if (root != 0) {
+		
+	}
+	else {
+		root = tNode;
+		current = root;
+	}
+	return false;
+}
+
+int getDepth(const int* treeLength){
+	int depth = 1;
+	int degree = 2;
+	while (*treeLength/degree>0)
+	{
+		degree *= 2;
+		depth++;
+	}
+	return depth;
 }
--- Ch7t7/Ch7t7.h
+++ Ch7t7/Ch7t7.h
@@ -1,3 +1,17 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+
+struct TNode
+{
+	std::string word;
+	int count;
+	TNode* left;
+	TNode* right;
+};
+
+bool addNode(TNode* tNode);
+void printNode(TNode* tNode);
+void printSortNode(TNode* tNode);
+int getDepth(const int* treeLength);
